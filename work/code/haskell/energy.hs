75b387682054674a792968dae7014cf0e4bc44a6
fibonaci <fibonaci>
2005-07-25 10:18:03 +0000

    r1958@jabberwock:  fibonaci | 2005-07-24 10:39:22 +0000  Added an interesting little teeny haskell program.

---
+++ work/code/haskell/energy.hs
@@ -0,0 +1,22 @@
+data Circuit where
+    Series   :: Circuit -> Circuit -> Circuit
+    Parallel :: Circuit -> Circuit -> Circuit
+    Resistor :: Int                -> Circuit
+
+
+reduce :: Circuit -> Int
+
+reduce (Series x y)   = gcd (reduce x) (reduce y)
+reduce (Parallel x y) = lcm (reduce x) (reduce y)
+reduce (Resistor r)   = r
+
+circuit :: Circuit
+circuit = 
+    Parallel
+        (Series
+            (Resistor 2)
+            (Resistor 4))
+        (Resistor 7)
+
+
+main = print $ reduce circuit

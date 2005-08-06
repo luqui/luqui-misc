3ba8a32df922926d441cf1748c5aa48d075fde3c
fibonaci <fibonaci>
2005-08-06 05:05:52 +0000

    r875@Queen-of-Hearts:  luqui | 2005-08-05 23:26:55 -0600  Added the beginning of a Monad module.

---
+++ work/code/perl/modules/Monad/lib/Monad.pm
@@ -0,0 +1,82 @@
+package Monad;
+
+use 5.006001;
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use Filter::Simple;
+use Text::Balanced qw<extract_codeblock>;
+use PPI;
+use PPI::Dumper;
+
+FILTER {
+    local %PPI::Token::Operator::OPERATOR = (
+        %PPI::Token::Operator::OPERATOR,
+        '<-' => 1,
+    );
+    $_ = filter($_);
+    if ($_[1] eq '-debug') {
+        print STDERR $_;
+    }
+    $_;
+};
+
+sub filter {
+    my ($text) = @_;
+    while ($text =~ /DO \s* (?= \{ )/gx) {
+        my $prepos = $-[0];
+        my ($extracted) = extract_codeblock $text;
+        my $postpos = pos $text;
+
+        my $monad = monadize($extracted);
+        substr($text, $prepos, $postpos - $prepos) = $monad;
+        pos $text = $postpos + length($monad) - length($extracted);
+    }
+    $text;
+}
+
+sub monadize {
+    my ($text) = @_;
+    $text = filter($text);
+    
+    my $doc = PPI::Document->new(\$text);
+    $doc->prune('PPI::Token::Comment');
+    my ($block) = $doc->children;
+    my @top = $block->children;
+    my $final = '{';
+    my $tail;
+
+    my $nstmts = grep { $_->isa('PPI::Statement') } @top;
+    my $stmtct = 0;
+
+    for (@top) {
+        if ($_->isa('PPI::Statement')) {
+            my $str = $_;
+            $str =~ s/;\s*$//;
+            if ($str =~ /^\s*\^/) {
+                $str =~ s/\^//;
+                $final .= "$str;";
+                $stmtct++;
+            }
+            elsif (++$stmtct == $nstmts) {
+                $final .= "$str";
+            }
+            elsif ($str =~ /<-/) {
+                my ($left, $right) = split /<-/, $str;
+                $final .= "do{$right}->BIND(sub { ($left) = \@_; ";
+                $tail .= "})";
+            }
+            else {
+                $final .= "do{$str}->BIND(sub { ";
+                $tail .= "})";
+            }
+        }
+        else {
+            $final .= $_;
+        }
+    }
+    $final .= "$tail}";
+}
+
+1;

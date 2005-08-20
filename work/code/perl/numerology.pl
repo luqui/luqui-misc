c84d2fb9f8c49faef3d9eedc78b9eec8dddd730b
fibonaci <fibonaci>
2005-08-20 12:10:54 +0000

    r2428@jabberwock:  fibonaci | 2005-08-17 21:56:16 +0000  Added a silly prime numerology script.

---
+++ work/code/perl/numerology.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use Math::Big::Factors qw<factors_wheel>;
+use List::Util qw<sum>;
+use Perl6::Say;
+
+sub reduction {
+    my ($num) = @_;
+    sum factors_wheel $num;
+}
+
+sub characteristic {
+    my ($num) = @_;
+    my $new;
+    until (($new = reduction $num) == $num) {
+        $num = $new;
+    }
+    $num;
+}
+
+while (<>) {
+    chomp;
+    say characteristic $_;
+}

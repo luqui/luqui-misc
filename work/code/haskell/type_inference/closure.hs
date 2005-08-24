17a9a0d7e49b31e9df3eb7be8689c5d15456de1f
fibonaci <fibonaci>
2005-08-24 01:13:57 +0000

    r1216@gamedev:  fibonaci | 2005-08-23 19:13:53 -0600  Switch to using set and queue types instead of mocking my own.

--- work/code/haskell/type_inference/closure.hs
+++ work/code/haskell/type_inference/closure.hs
@@ -1,30 +1,28 @@
 -- from code/perl/type_inference/closure.pl
 
 import Control.Monad.State
+import qualified Data.Set as Set
+import Data.Set (Set)
+import Data.Queue
+
 
 data Type = Type String
           | List Type
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 data Rule = Does Type Type
-    deriving (Show, Eq)
-
-rules :: [Rule]
-rules = [
-    Does (Type "a") (List $ Type "a"),
-    Does (List $ List $ Type "a") (Type "Num"),
-    Does (Type "c") (Type "a") ]
+    deriving (Show, Eq, Ord)
 
 -- Fall back on procedural methods, because I don't know how to think
 -- this functionally :-(
 
-data CS = CS [Rule] [Rule]
+data CS = CS (Queue Rule) (Set Rule)
 
 insert_rule :: Rule -> State CS ()
 insert_rule rule = do
     CS queue closure <- get
-    unless (rule `elem` closure) $ do
-        put (CS (queue ++ [rule]) (rule:closure))
+    unless (rule `Set.member` closure) $ do
+        put (CS (addToQueue queue rule) (Set.insert rule closure))
 
 replace :: Type -> Type -> Type -> Maybe Type
 replace typ pat repl 
@@ -38,7 +36,7 @@ process rule = do
     sequence_ $ map (\subrule -> do
                         luni subrule rule
                         runi subrule rule) 
-                    closure
+                    (Set.elems closure)
     where
     luni :: Rule -> Rule -> State CS ()
     luni (Does subrulel subruler) (Does t@(Type _) ruler) = 
@@ -54,16 +52,25 @@ process rule = do
               (replace subruler rulel t)
     runi _ _ = return ()
 
-close :: State CS [Rule]
-close = do
+close_ :: State CS (Set Rule)
+close_ = do
     CS queue closure <- get
-    if length queue > 0
-        then let (q:qs) = queue in do
-            put (CS qs closure)
-            process q
-            close
-        else return closure
+    maybe (return closure)
+          (\(e, newq) -> do
+              put (CS newq closure)
+              process e
+              close_)
+          (deQueue queue)
+
+close :: [Rule] -> [Rule]
+close rules = Set.toList $ fst $ runState close_ $ 
+                CS (listToQueue rules) (Set.fromList rules)
 
 main :: IO ()
-main = let closure = fst $ runState close $ CS rules rules in
-           sequence_ (map print closure)
+main = sequence_ $ map print $ close rules
+    where
+    rules :: [Rule]
+    rules = [
+        Does (Type "a") (List $ Type "a"),
+        Does (List $ List $ Type "a") (Type "Num"),
+        Does (Type "c") (Type "a") ]

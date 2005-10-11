3b6b176ab1d57506b10cb2149fa1b232735a7971
fibonaci <fibonaci>
2005-10-11 19:09:20 +0000

    r1240@ec237-54-dhcp:  luqui | 2005-10-11 13:34:52 -0600  Rec (LF a) ~=~ [a]

--- work/code/haskell/cata.hs
+++ work/code/haskell/cata.hs
@@ -5,3 +5,9 @@ cata f (In a) = f (fmap (cata f) a)
 
 ana :: (Functor f) => (a -> f a) -> a -> Rec f
 ana f a = In $ fmap (ana f) $ f a
+
+data LF a lf = Nil | Cons a lf
+
+instance Functor (LF a) where
+    fmap f Nil = Nil
+    fmap f (Cons a b) = Cons a (f b)

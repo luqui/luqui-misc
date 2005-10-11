1a3fa617f79c327b1417b4f2fc977116c1b14bba
fibonaci <fibonaci>
2005-10-11 05:03:56 +0000

    r1237@Queen-of-Hearts:  luqui | 2005-10-10 23:29:23 -0600  Anamorphism!

--- work/code/haskell/cata.hs
+++ work/code/haskell/cata.hs
@@ -1,3 +1,7 @@
 data Rec f = In { out :: f (Rec f) }
 
+cata :: (Functor f) => (f a -> a) -> Rec f -> a
 cata f (In a) = f (fmap (cata f) a)
+
+ana :: (Functor f) => (a -> f a) -> a -> Rec f
+ana f a = In $ fmap (ana f) $ f a

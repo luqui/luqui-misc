ba78fdf282f89170e6ce852b1a02dabd649cb844
fibonaci <fibonaci>
2005-10-21 23:59:25 +0000

    r1294@ec237-62-dhcp:  luqui | 2005-10-21 18:25:33 -0600  Docs for Thunk.pm.

--- work/code/perl/modules/Language-AttributeGrammar/lib/Language/AttributeGrammar/Thunk.pm
+++ work/code/perl/modules/Language-AttributeGrammar/lib/Language/AttributeGrammar/Thunk.pm
@@ -3,10 +3,22 @@ package Language::AttributeGrammar::Thunk;
 use Carp::Clan '^Language::AttributeGrammar';
 use Perl6::Attributes;
 
-# three-stage thunk
-#   stage 1: code unset
-#   stage 2: code set, unevaluated
-#   stage 3: code evaluated and return value stored
+=head1 TITLE
+
+Language::AttributeGrammar::Thunk - Delayed code logic
+
+=head1 DESCRIPTION
+
+This is a four stage thunk.
+
+    stage 1: code unset
+    stage 2: code set, unevaluated
+    stage 3: code being evaluated
+    stage 4: code evaluated and return value stored
+
+=over
+
+=cut
 
 sub new {
     my ($class, $code, $attr, $at) = @_;
@@ -20,6 +32,13 @@ sub new {
     $self;
 }
 
+=item * new($class, ?$code, ?$attr, ?$at)
+
+Creates a new thunk object.  If $code, $attr, and $at are specified,
+initializes the object via C<set>.
+
+=cut
+
 sub set {
     my ($self, $code, $attr, $at) = @_;
     unless ($.stage == 1) {
@@ -29,6 +48,16 @@ sub set {
     $.code = $code;
     $.stage++;
 }
+
+=item * $thunk->set($code, $attr, $at)
+
+Set the code for a thunk.  $attr is the name of the attribute that this code is
+evaluating (for example "Cons:length") and $at is a description of the location
+at which the thunk was defined (for example "grammar.pl line 42").  Both of the
+latter two are only used for diagnostic purposes.  This method is only valid
+when the thunk is in stage 1 (and it moves it to stage 2).
+
+=cut
 
 sub get {
     my ($self, $attr, $at) = @_;
@@ -50,4 +79,20 @@ sub get {
     }
 }
 
+=item * $thunk->get($attr, $at)
+
+valuate a thunk.  $attr is the name of the attribute that is being fetched,
+and $at is a description of the location at which it is being fetched.  In
+stage 1 it fails because there is no code to evaluate, and in stage 3 it fails
+because this implies that an infinite loop would occur.  After successful
+execution of this method, the thunk is in stage 4.
+
+=cut
+
 1;
+
+=back
+
+=head1 SEE ALSO
+
+L<Language::AttributeGrammar>, L<Data::Lazy>

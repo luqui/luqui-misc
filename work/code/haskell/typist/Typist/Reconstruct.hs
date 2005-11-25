3044bb47811ab1b98610a0b22c038d9a6585b23f
fibonaci <fibonaci>
2005-11-25 09:30:40 +0000

    r1000@jabberwock:  luqui | 2005-11-25 03:00:10 +0000  Slow going progress.

--- work/code/haskell/typist/Typist/Reconstruct.hs
+++ work/code/haskell/typist/Typist/Reconstruct.hs
@@ -54,20 +54,50 @@ typeAST lam@(ExpLambda {})
 
 -- rule (abs_tp)
 typeAST lam@(ExpLambda {})
-        inh@(InhT (Just (TFunc tvars from to))) = do
+        inh@(InhT (Just (TFunc tvars dom rng))) = do
     bodytype <- local (Map.insert (lamParam lam) (lamParamT lam)) $
-                    typeAST (lamBody lam) to
+                    typeAST (lamBody lam) rng
     generalize (SynT $ TFunc tvars (lamParamT lam) bodytype) inh
 
 -- rule (abs)
 typeAST lam@(Lambda {})
-        inh@(InhT (Just (TFunc tvars from to))) = do
-    synfrom <- instantiate from
-    bodytype <- local (Map.insert (lamParam lam) synfrom) $
-                    typeAST (lamBody lam) to
-    synfrom <- instantiate from
-    return $ SynT $ TFunc tvars synfrom bodytype
+        inh@(InhT (Just (TFunc tvars dom rng))) = do
+    syndom <- instantiate dom
+    bodytype <- local (Map.insert (lamParam lam) syndom) $
+                    typeAST (lamBody lam) rng
+    return $ SynT $ TFunc tvars syndom bodytype
     
+-- rule (app_tp) and (app_(tp,_|_))
+typeAST app@(ExpApp {})
+        inh = do
+    funtype <- typeAST (appFun app) (InhT Nothing)
+    ressyn <- case funtype of
+        SynT (TFunc tvars dom rng) -> appTp (tvars,dom,rng)
+        SynT TBottom -> appTpBot
+        x -> fail $ "Trying to apply something that doesn't think it's a function: " ++ show x
+
+    generalize ressyn inh
+    
+    where
+
+    appTp :: ([TypeID], SynT, SynT) -> Ann SynT
+    appTp (tvars, dom, rng) = do
+        when (length (appTArg app) /= length tvars) $
+            fail $ "Can't apply " ++ show (length (appTArg app))
+                    ++ " varaibles to a function expecting " ++ show (length tvars)
+        ctxtype <- substitute (zip (appTArg app) tvars) dom
+        ctx <- destantiate ctxtype
+        argtype <- typeAST (appArg app) ctx
+        
+        substitute (zip (appTArg app) tvars) rng
+
+    appTpBot :: Ann SynT
+    appTpBot = do
+        typeAST (appArg app) (InhT (Just TTop))
+        return (SynT TBottom)
+
+-- rule (app_(tp,_|_))
+
 typeAST ast inh = fail $ "Can't type (" ++ show ast ++ ") with context (" ++ show inh ++ ")"
 
 
@@ -76,6 +106,11 @@ typeAST ast inh = fail $ "Can't type (" ++ show ast ++ ") with context (" ++ sho
 -- what to do when the pattern has holes??
 instantiate :: InhT -> Ann SynT
 instantiate = undefined
+
+-- take a SynT type and create a fully defined InhT
+-- pattern
+destantiate :: SynT -> Ann InhT
+destantiate = undefined
 
 -- The up-right arrow operation in the paper
 -- Match a SynT against an InhT pattern covariantly
@@ -86,3 +121,7 @@ generalize = undefined
 -- Match a SynT against an InhT pattern contravariantly
 specify :: SynT -> InhT -> Ann SynT
 specify = undefined
+
+
+substitute :: [(SynT, TypeID)] -> SynT -> Ann SynT
+substitute = undefined

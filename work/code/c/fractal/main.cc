3ca66fe09705c40cdeafdcfef85f3595af9f9035
fibonaci <fibonaci>
2005-11-27 01:40:56 +0000

    r1085@jabberwock:  luqui | 2005-11-26 19:09:28 +0000  Fractaly terrainy stuff.

--- work/code/c/fractal/main.cc
+++ work/code/c/fractal/main.cc
@@ -9,26 +9,37 @@
 
 typedef float num;
 
-const int pix_width  = 640;
-const int pix_height = 480;
+const int buf_width  = 200;
+const int buf_height = 200;
 const num screen_left = -2;
 const num screen_right = 1;
 const num screen_bottom = -1;
 const num screen_top = 1;
 
-num r = 1;
+num buffer[buf_width][buf_height];
 
-void draw_mandel();
+typedef num buf_t[buf_width][buf_height];
+
+void fill_buffer(buf_t, num, num, num, num);
+void draw_buffer(buf_t, num, num, num, num);
+
+num camerax = 0;
+num cameray = 0;
+num cameraz = -1;
+num camerastep = 0.05;
 
 SDL_Surface* screen;
 void init_sdl() {
     SDL_Init(SDL_INIT_VIDEO);
-    screen = SDL_SetVideoMode(pix_width, pix_height, 32, SDL_OPENGL);
+    screen = SDL_SetVideoMode(800, 600, 32, SDL_OPENGL);
 
     glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        gluOrtho2D(screen_left, screen_right, screen_bottom, screen_top);
+        gluPerspective(45.0, 800.0/600.0, 0.1, 100);
     glMatrixMode(GL_MODELVIEW);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
 }
 
 void quit() {
@@ -46,17 +57,25 @@ void events() {
             break;
         }
     }
+
+    Uint8* keys = SDL_GetKeyState(NULL);
+    if (keys[SDLK_a]) camerax -= camerastep;
+    if (keys[SDLK_d]) camerax += camerastep;
+    if (keys[SDLK_w]) cameraz += camerastep;
+    if (keys[SDLK_s]) cameraz -= camerastep;
 }
 
 int main(int argc, char** argv) {
     init_sdl();
 
-    num rs = -M_PI/2;
-    glClear(GL_COLOR_BUFFER_BIT);
+    fill_buffer(buffer, screen_left, screen_right, screen_bottom, screen_top);
     for (;;) {
-        r = 2*sin(rs);
-        rs += 0.01;
-        draw_mandel();
+        glLoadIdentity();
+        gluLookAt(camerax, cameray, cameraz,
+                  0, 0, 10,
+                  0, 1, 0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        draw_buffer(buffer, screen_left, screen_right, screen_bottom, screen_top);
         SDL_GL_SwapBuffers();
         events();
     }
@@ -64,29 +83,62 @@ int main(int argc, char** argv) {
     quit();
 }
 
-int mandel_order(num cr, num ci, int limit) {
+num mandel_order(num cr, num ci, int limit) {
     int iters = 0;
     num zr = 0, zi = 0, rsq, isq;
     while (++iters < limit) {
         if ((isq = zi*zi) + (rsq=zr*zr) > 4) break;
         zi = ci + 2*zr*zi;
-        zr = cr - isq + r*rsq;
+        zr = cr - isq + rsq;
     }
-    if (iters == limit) return 0;
+    if (iters == limit) return 300;
     else return iters;
 }
 
-void draw_mandel() {
-    num rstep = (screen_right - screen_left) / pix_width;
-    num istep = (screen_top - screen_bottom) / pix_height;
+void fill_buffer(buf_t buffer, num left, num right, num bottom, num top)
+{
+    num rstep = (right - left) / buf_width;
+    num istep = (top - bottom) / buf_height;
 
-    glBegin(GL_POINTS);
-    for (num r = screen_left; r <= screen_right; r += rstep) {
-        for (num i = screen_bottom; i <= screen_top; i += istep) {
-            int order = 30 * mandel_order(r, i, 200) % 256;
-            glColor3ub(order, order, order);
-            glVertex2d(r, i);
+    int rind, iind;
+    num r, i;
+    
+    for (r = left, rind = 0; rind < buf_width; r += rstep, rind++) {
+        for (i = bottom, iind = 0; iind < buf_height; i += istep, iind++) {
+            buffer[rind][iind] = log(mandel_order(r, i, 200) + 0.5);
         }
     }
-    glEnd();
+}
+
+void itercolor(num iters) { 
+    if (iters > log(200.0)) {
+        glColor3d(0,0,0);
+    }
+    else {
+        glColor3d(sin(10*iters), cos(10*iters), 1);
+    }
+}
+
+void draw_buffer(buf_t buffer, num left, num right, num bottom, num top)
+{
+    num rstep = (right - left) / buf_width;
+    num istep = (top - bottom) / buf_height;
+    
+    int rind, iind;
+    num r, i;
+
+    for (r = left, rind = 0; rind < buf_width-1; r += rstep, rind++) {
+        for (i = bottom, iind = 0; iind < buf_height-1; i += istep, iind++) {
+            glBegin(GL_QUADS);
+                itercolor(buffer[rind][iind]);
+                glVertex3d(r, i, buffer[rind][iind]);
+                itercolor(buffer[rind+1][iind]);
+                glVertex3d(r + rstep, i, buffer[rind+1][iind]);
+                itercolor(buffer[rind+1][iind+1]);
+                glVertex3d(r + rstep, i + istep, buffer[rind+1][iind+1]);
+                itercolor(buffer[rind][iind+1]);
+                glVertex3d(r, i + istep, buffer[rind][iind+1]);
+            glEnd();
+        }
+    }
 }

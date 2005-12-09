2974b2b855e7e66e89acc69501facbc986f0810f
fibonaci <fibonaci>
2005-12-09 08:50:14 +0000

    r1386@jabberwock:  luqui | 2005-12-09 02:19:31 +0000  Stuff.

--- work/code/haskell/automata/Automata/Analyze.hs
+++ work/code/haskell/automata/Automata/Analyze.hs
@@ -3,6 +3,8 @@
 module Automata.Analyze where
 
 import Data.Array
+import System.Random
+import Control.Monad.State
 
 -- A little bit of mathematical hackery here. Since x ln x goes to 
 -- zero when x does, we pretend that x ln y goes to zero when x does,
@@ -21,3 +23,40 @@ mutualInformation probs =
     pi i = sum [ probs ! (i,j) | j <- [jmin..jmax] ]
     pj j = sum [ probs ! (i,j) | i <- [imin..imax] ]
     ((imin, jmin), (imax, jmax)) = bounds probs
+
+-- XXX this is only well-defined for size bits = 2^size inp
+-- but I don't do any error checking :-(
+binaryRule :: Array Int Int -> ([Int] -> Int)
+binaryRule bits inp = binaryRule' bitsMin (bitsMax+1) inp
+    where
+    binaryRule' min max [] = bits ! min
+    binaryRule' min max (x:xs) = 
+        case x of
+            0 -> binaryRule' min (avg min max) xs
+            1 -> binaryRule' (avg min max) max xs
+    (bitsMin, bitsMax) = bounds bits
+    avg x y = (x + y) `div` 2
+
+binaryPath :: Int -> [Int] -> [Array Int Int]
+binaryPath size []     = [listArray (0,size-1) $ repeat 1]
+binaryPath size (p:ps) = 
+    let prev = binaryPath size ps in
+    (head prev // [(p,0)]) : prev
+
+randomRM :: (RandomGen g, Random a) => (a,a) -> State g a
+randomRM rng = do
+    gen <- get
+    let (ret, gen') = randomR rng gen
+    put gen'
+    return ret
+
+pickFrom :: (RandomGen g) => [a] -> State g (a,[a])
+pickFrom xs = do
+    idx <- randomRM (0, length xs - 1)
+    return (xs !! idx, take idx xs ++ drop (idx+1) xs)
+
+permute :: (RandomGen g) => [a] -> State g [a]
+permute [] = return []
+permute xs = do
+    (r, rs) <- pickFrom xs
+    fmap (r :) $ permute rs

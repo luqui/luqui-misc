81077d665018e4d1d7c8d56301aeb905b5c16650
fibonaci <fibonaci>
2006-01-06 00:10:52 +0000

    r2362@jabberwock:  luqui | 2006-01-05 17:41:17 +0000  Forgot to add two source files.

---
+++ work/code/games/ropes/drawing.cc
@@ -0,0 +1,36 @@
+#include "drawing.h"
+
+void draw_circle() {
+    static int dlist = 0;
+    if (dlist) {
+        glCallList(dlist);
+    }
+    else {
+        dlist = glGenLists(1);
+        glNewList(dlist, GL_COMPILE_AND_EXECUTE);
+            num theta = 0;
+            glBegin(GL_TRIANGLE_FAN);
+                glVertex2d(0,0);
+                for (int i = 0; i <= 24; i++, theta += M_PI/12) {
+                    glVertex2d(cos(theta), sin(theta));
+                }
+            glEnd();
+        glEndList();
+    }
+}
+
+void draw_circle(num radius) {
+    glPushMatrix();
+    glScaled(radius, radius, 0);
+    draw_circle();
+    glPopMatrix();
+}
+
+void draw_box(vec ll, vec ur) {
+    glBegin(GL_QUADS);
+        glVertex2d(ll.x, ll.y);
+        glVertex2d(ur.x, ll.y);
+        glVertex2d(ur.x, ur.y);
+        glVertex2d(ll.x, ur.y);
+    glEnd();
+}
---
+++ work/code/games/ropes/objects.cc
@@ -0,0 +1,94 @@
+#include "objects.h"
+
+void ObjectManager::mark(Object* o) {
+    if (o->visible_) return;
+    o->visible_ = true;
+    
+    if (gc_tail_) gc_tail_->gc_next_ = o;
+    o->gc_next_ = 0;
+    gc_tail_ = o;
+}
+
+void ObjectManager::sweep() {
+    Object* cptr = 0;
+    
+    gc_tail_ = 0;
+    for (set_t::iterator i = oset_.begin(); i != oset_.end(); ++i) {
+        (*i)->visible_ = false;
+        if ((*i)->visible()) {
+            if (!cptr) cptr = *i;
+            mark(*i);
+        }
+    }
+
+    while (cptr) {
+        cptr->mark();
+        cptr = cptr->gc_next_;
+    }
+
+    int cleaned = 0;
+    for (set_t::iterator i = oset_.begin(); i != oset_.end(); ) {
+        Object* o = *i;
+        if (!o->visible_) {
+            set_t::iterator next = i;  ++next;
+            oset_.erase(i);
+            i = next;
+
+            delete o;
+            cleaned++;
+        }
+        else {
+            ++i;
+        }
+    }
+
+    if (cleaned) cout << "Cleaned up " << cleaned << " objects\n";
+}
+
+const num Spikey::radius = 0.2;
+
+Spikey::Spikey(vec p, vec f)
+    : geom_(p, radius)
+{
+    body_.set_position(p);
+    body_.set_owner(static_cast<void*>(this));
+    geom_.attach(&body_);
+    body_.set_mass(0.1, radius);
+
+    body_.apply_force(f, p);
+}
+
+Rope::Rope(Object* obja, Body* bodya, Object* objb, Body* bodyb)
+    : obja_(obja), objb_(objb)
+{
+    vec apos = bodya->position();
+    proxya_.set_position(apos);
+    proxya_.set_velocity(bodya->velocity());
+    proxya_.set_mass(0.1, 1);
+    hingea_ = dJointCreateHinge(WORLD, 0);
+    dJointAttach(hingea_, proxya_.body_id(), bodya->body_id());
+    dJointSetHingeAxis(hingea_, 0, 0, 1);
+
+    vec bpos = bodyb->position();
+    proxyb_.set_position(bpos);
+    proxyb_.set_velocity(bodyb->velocity());
+    proxyb_.set_mass(0.1, 1);
+    hingeb_ = dJointCreateHinge(WORLD, 0);
+    dJointAttach(hingeb_, proxyb_.body_id(), bodyb->body_id());
+    dJointSetHingeAxis(hingeb_, 0, 0, 1);
+    
+    rope_ = dJointCreateSlider(WORLD, 0);
+    vec axis = bpos - apos;
+    dJointAttach(rope_, proxya_.body_id(), proxyb_.body_id());
+    dJointSetSliderAxis(rope_, axis.x, axis.y, 0);
+    
+    dJointSetSliderParam(rope_, dParamLoStop, 0);
+    dJointSetSliderParam(rope_, dParamStopCFM, 0.25);
+    dJointSetSliderParam(rope_, dParamStopERP, 0.01);
+}
+
+Rope::~Rope() {
+    dJointDestroy(hingea_);
+    dJointDestroy(hingeb_);
+    dJointDestroy(rope_);
+}

a44c31c6d2b1ddce098e5460dc25dcb0eb2c6c3f
fibonaci <fibonaci>
2006-02-10 19:45:49 +0000

    Nextu Problemu.

--- work/class/MATH_4650/2006-02-04/converge.pl
+++ work/class/MATH_4650/2006-02-04/converge.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use Perl6::Placeholders;
+use Term::ReadLine;
 
 sub checkzero {
     my ($f, $p) = @_;
@@ -16,30 +16,13 @@ sub checkconv {
     return abs(($a-$b)/$a) < 1e-12;
 }
 
-sub bisection {
-    my ($f, $lo, $hi) = @_;
-    die "bisection: Usage: bisection(func, lower bound, upper bound)\n"
-        unless $f->($lo) < 0 && $f->($hi) > 0;
-
-    my $mid  = $hi;
-    my $prev;
-    while (1) {
-        $prev = $mid;
-        $mid = ($hi+$lo)/2;
-        
-        if (checkconv($mid, $prev)) { last; }
-        elsif ($f->($mid) < 0)      { $lo = $mid; }
-        else                        { $hi = $mid; }
-    }
-    return checkzero($f, $mid);
-}
-
 sub newton {
     my ($f, $df, $p0) = @_;
     my $p = $p0;
     my $prev;
     
     while (1) {
+        print "  $p\n";
         $prev = $p;
         $p -= $f->($p) / $df->($p);
         
@@ -49,5 +32,16 @@ sub newton {
 }
 
 my $pi = 4*atan2(1,1);
-print bisection({ sin($^x) }, 3*$pi/2, $pi/2), "\n";
-print newton({ sin($^x) }, { cos($^x) }, $pi/2+0.5), "\n";
+
+my $term = Term::ReadLine::Gnu->new;
+
+my $fntxt = $term->readline('f($x) = ');
+my $fn = eval "sub { my \$x = shift; $fntxt }" 
+            or die "Failed parse: $@";
+my $fptxt = $term->readline('f\'($x) = ');
+my $fp = eval "sub { my \$x = shift; $fptxt }" 
+            or die "Failed parse: $@";
+
+while (my $intxt = $term->readline('Initial guess = ')) {
+    print newton($fn, $fp, $intxt), "\n";
+}

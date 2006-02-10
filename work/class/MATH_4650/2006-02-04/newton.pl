51c04ab1437ee210cdf3ff69a04c6f183a4e7b86
fibonaci <fibonaci>
2006-02-10 19:45:50 +0000

    next problem.

--- work/class/MATH_4650/2006-02-04/newton.pl
+++ work/class/MATH_4650/2006-02-04/newton.pl
@@ -31,6 +31,21 @@ sub newton {
     return checkzero($f, $p);
 }
 
+sub secant {
+    # basically straight from the book
+    my ($f, $p0, $p1) = @_;
+    my ($q0, $q1) = ($f->($p0), $f->($p1));
+    my $p;
+    
+    while (1) {
+        print "  $p0, $p1\n";
+        $p = $p1 - $q1 * ($p1 - $p0) / ($q1 - $q0);
+        last if checkconv($p, $p1);
+        ($p0, $q0, $p1, $q1) = ($p1, $q1, $p, $f->($p));
+    }
+    return checkzero($f, $p);
+}
+
 my $pi = 4*atan2(1,1);
 
 my $term = Term::ReadLine::Gnu->new;
@@ -43,5 +58,11 @@ my $fp = eval "sub { my \$x = shift; $fptxt }"
             or die "Failed parse: $@";
 
 while (my $intxt = $term->readline('Initial guess = ')) {
-    print newton($fn, $fp, $intxt), "\n";
+    if ($intxt =~ s/^secant\s*//) {
+        my $intxt2 = $term->readline('Initial guess 2 = ');
+        print secant($fn, $intxt, $intxt2), "\n";
+    }
+    else {
+        print newton($fn, $fp, $intxt), "\n";
+    }
 }

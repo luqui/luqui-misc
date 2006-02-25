a1f6b1cb577c462457a4a58d9a508dbfc3f5f712
fibonaci <fibonaci>
2006-02-25 03:44:24 +0000

    Added output to the Neville's method program.

--- work/class/MATH_4650/2006-02-23/neville.pl
+++ work/class/MATH_4650/2006-02-23/neville.pl
@@ -34,3 +34,31 @@ while (defined(my $line = $term->readline('Enter the next point x,f(x): '))) {
     my ($x, $fx) = split /\s*,\s*/, $line;
     print $nev->($x,$fx), "\n";
 }
+
+__END__
+
+OUTPUT:
+
+At what point would you like the function evaluated? 3.7
+Enter the next point x,f(x): 1,-0.841471
+-0.841471
+Enter the next point x,f(x): 3.2,1.22152
+1.69038159090909
+Enter the next point x,f(x): 3.3,1.35167
+1.90390276679842
+Enter the next point x,f(x): 4,2.1431
+1.83698399096556
+Enter the next point x,f(x): 0.5,-1.17257
+1.84364754217956
+Enter the next point x,f(x): 2.1,-0.121272
+1.83736920043875
+
+----------
+
+At what point would you like the function evaluated? 3.7
+Enter the next point x,f(x): 4,2.1431
+2.1431
+Enter the next point x,f(x): 0.5,-1.17257
+1.85889971428571
+Enter the next point x,f(x): 2.1,-0.121272
+1.71223544360902

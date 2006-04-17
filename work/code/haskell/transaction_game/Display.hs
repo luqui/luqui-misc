04fdd8e1bbd8e281500d146bb7bdbeaff54fa7a3
fibonaci <fibonaci>
2006-04-17 12:36:14 +0000

    Drawing outlines.

--- work/code/haskell/transaction_game/Display.hs
+++ work/code/haskell/transaction_game/Display.hs
@@ -20,21 +20,36 @@ empty = VertexMap (Map.empty)
 addVertex :: Graph.Vertex -> (Double,Double) -> VertexMap -> VertexMap
 addVertex v p (VertexMap m) = VertexMap $ Map.insert v p m
 
+rectPoints :: (Double,Double) -> (Double,Double) -> IO ()
+rectPoints (x,y) (x',y') = do
+	GL.vertex $ GL.Vertex2 x y
+	GL.vertex $ GL.Vertex2 x' y
+	GL.vertex $ GL.Vertex2 x' y'
+	GL.vertex $ GL.Vertex2 x y'
+
 drawVertex :: (Double,Double) -> IO ()
 drawVertex (x,y) = do
+	GL.color black
+	GL.rect (GL.Vertex2 (x-0.48) (y-0.48)) (GL.Vertex2 (x+0.48) (y+0.48))
 	GL.color white
-	GL.rect (GL.Vertex2 (x-0.5) (y-0.5)) (GL.Vertex2 (x+0.5) (y+0.5))
+	GL.renderPrimitive GL.LineLoop $ rectPoints (x-0.5, y-0.5) (x+0.5, y+0.5)
+	where
+	white :: GL.Color3 Double
+	white = GL.Color3 1 1 1
+	black :: GL.Color3 Double
+	black = GL.Color3 0 0 0
+
+drawLine :: (Double,Double) -> (Double,Double) -> IO ()
+drawLine (x,y) (x',y') = do
+	GL.color white
+	GL.renderPrimitive GL.Lines $ do
+		GL.vertex $ GL.Vertex2 x y
+		GL.vertex $ GL.Vertex2 x' y'
 	where
 	white :: GL.Color3 Double
 	white = GL.Color3 1 1 1
 
-drawLine :: (Double,Double) -> (Double,Double) -> IO ()
-drawLine (x,y) (x',y') =
-	GL.renderPrimitive GL.Lines $ do
-		GL.vertex $ GL.Vertex2 x y
-		GL.vertex $ GL.Vertex2 x' y'
-
 displayGraph :: VertexMap -> Graph.Graph -> IO ()
 displayGraph (VertexMap vtxs) g = do
-	mapM_ (drawVertex . (vtxs Map.!)) (Graph.vertices g)
 	mapM_ (\ (u,v) -> drawLine (vtxs Map.! u) (vtxs Map.! v)) (Graph.edges g)
+	mapM_ (drawVertex . (vtxs Map.!)) (Graph.vertices g)

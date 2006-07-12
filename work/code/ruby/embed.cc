c05fbb026bd2a0fc07fd84d915aad4272537ebad
fibonaci <fibonaci>
2006-07-12 15:42:56 +0000

    I know how to embed ruby, w00t.

--- work/code/ruby/embed.cc
+++ work/code/ruby/embed.cc
@@ -1,9 +1,25 @@
 #include "ruby.h"
+#include <iostream>
+
+typedef VALUE (*rb_fp)(...);
+
+template <class Ty>
+rb_fp rb_fpcast(Ty in) {
+	return reinterpret_cast<rb_fp>(in);
+}
+
+VALUE add_one(VALUE self, VALUE num)
+{
+	return INT2NUM(NUM2INT(num) + 1);
+}
 
 int main()
 {
     ruby_init();
     ruby_script("embed");
+
+	rb_define_global_function("add_one", rb_fpcast(&add_one), 1);
+	
     rb_load_file("embed.rb");
     ruby_run();
 }
--- work/code/ruby/embed.rb
+++ work/code/ruby/embed.rb
@@ -1,1 +1,2 @@
 puts "Whatup, dawg"
+puts "Three plus one is " + add_one(3).to_s

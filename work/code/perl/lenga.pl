5fe6827a433793c7af07bd01e25afa7ff439e3a4
fibonaci <fibonaci>
2006-09-05 22:17:32 +0000

    more advanced lenga interface.

--- work/code/perl/lenga.pl
+++ work/code/perl/lenga.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use Term::ReadLine;
+use IPC::Open2;
 use List::Util qw<max>;
 
 my @STMTS;
@@ -11,12 +12,22 @@ my $term = Term::ReadLine->new('Lenga');
 sub prove {
 	my ($t, @stmts) = @_;
 	
-	my $list = join '', map { "$_.\n" } @stmts;
-	open my $fh, '| otter >& /dev/null';
-	print $fh "set(auto).\nassign(max_seconds,$t).\nformula_list(usable).\n";
-	print $fh $list;
-	print $fh "end_of_list.\n";
-	close $fh;
+	my $list = join '', map { " " x 8 . "$_.\n" } @stmts;
+	my $pid = open2(my $out, my $in, 'otter 2>/dev/null');
+	
+	print $in <<PRELUDE;
+      set(auto).
+      assign(max_seconds,$t).
+      formula_list(usable).
+	    all x (x = x).
+PRELUDE
+	print $in $list;
+	print $in "end_of_list.\n";
+	close $in;
+
+	my $outs = join '', <$out>;
+	close $out;
+	waitpid($pid, 0);
 	
 	return 1 if $? >> 8 == 103;   # inconsistent (proved)
 	return 0 if $? >> 8 == 104;   # consistent (not proved)
@@ -24,40 +35,81 @@ sub prove {
 		print "  \e[31mMax search time exceeded.  Assuming consistency.\e[0m\n";
 		return 0;
 	}
-	die "Otter did something naughty ($! ; $?)\n";
+	die "Otter did something naughty ($! ; $?):\n$outs";
 }
 
 sub check_consistency {
 	unless (prove(15, @STMTS)) {
-		print "\e[1;32mTheory consistent.\e[0m\n";
+		print "\e[1;32mTheory consistent.\e[0m\n";  1;
 	}
 	else {
-		print "\e[1;33mTheory inconsistent.\e[0m\n";
+		print "\e[1;33mTheory inconsistent.\e[0m\n";  0;
 	}
 }
 
 sub check_independence {
-	return if @STMTS <= 1;
-	for my $i (reverse 0..$#STMTS) {
-		my @stmts = @STMTS;
-		$stmts[$i] = "-($stmts[$i])";
-		if (prove(max(int(15/@stmts),1), @stmts)) {
-			print "\e[1;33mThe axiom '$STMTS[$i]' is a theorem\e[0m\n";
-			splice @STMTS, $i, 1;
-			return check_independence();
-		}
+	my ($stmtnum) = @_;
+	return if @STMTS == 0;
+	my @stmts = @STMTS;
+	$stmts[$stmtnum] = "-($stmts[$stmtnum])";
+	if (prove(max(int(15/@stmts),1), @stmts)) {
+		print "\e[1;33mThe axiom '$STMTS[$stmtnum]' is a theorem\e[0m\n";
+		splice @STMTS, $stmtnum, 1;
+		return 0;
+	}
+	else {
+		return 1;
 	}
 }
 
-while (defined($_ = $term->readline('> '))) {
-	my @oldstmts = @STMTS;
-	push @STMTS, $_;
+sub errcheck(&) {
+	my ($code) = @_;
 	eval {
-		check_consistency();
-		check_independence();
-		1;
+		$code->(); 1;
 	} or do {
 		print "\e[1;31mThere was some kind of error ($@).\e[0m\n";
-		@STMTS = @oldstmts;
+		return 0;
 	};
+	return 1;
+}
+
+my $AUTOCHECK = 0;
+
+while (defined($_ = $term->readline('> '))) {
+	if ($_ =~ /^\s*list\s*$/) {
+		print "$_:  $STMTS[$_]\n" for 0..$#STMTS;
+		next;
+	}
+	elsif ($_ =~ /^\s*inconsistent\s*$/) {
+		errcheck { check_consistency() };
+	}
+	elsif ($_ =~ /^\s*theorem (\d+)\s*$/) {
+		errcheck { check_independence($1) and print "Nope.\n" };
+	}
+	elsif ($_ =~ /^\s*kill (\d+)\s*$/) {
+		splice @STMTS, $1, 1;
+	}
+	elsif ($_ =~ /^\s*autocheck\s*$/) {
+		$AUTOCHECK = !$AUTOCHECK;
+		print "Autocheck " . ($AUTOCHECK ? "on" : "off") . "\n";
+	}
+	else {
+		my @oldstmts = @STMTS;
+		push @STMTS, $_;
+
+		if ($AUTOCHECK) {
+			errcheck { 
+				if (check_consistency()) {
+					check_independence();
+				}
+				else {
+					print "I'm going to pretend you didn't say that...\n";
+					@STMTS = @oldstmts;
+				}
+			} or do {
+				print "I'm going to pretend you didn't say that...\n";
+				@STMTS = @oldstmts;
+			};
+		}
+	}
 }

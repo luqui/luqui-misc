4e205af868c55fd27cd9050a1715d7b438d242ce
fibonaci <fibonaci>
2007-01-22 09:38:57 +0000

    Oh  my god it freakin works sortof!

--- work/code/haskell/generalize.hs
+++ work/code/haskell/generalize.hs
@@ -10,7 +10,7 @@ data Type where
     TAtom  :: String             -> Type  -- Int, Top, etc.
     TArrow :: Type -> Type       -> Type  -- functions
     TTuple :: Type -> Type       -> Type  -- tuples
-    TVar   :: Integer            -> Type  -- singular types
+    TVar   :: Integer -> Bool    -> Type  -- singular types
     TSup   :: Integer            -> Type  -- supremum type (base, inst)
     TInf   :: Integer            -> Type  -- infimum type  (base, inst)
     TLam   :: Integer -> Type    -> Type  -- universal types
@@ -20,7 +20,7 @@ instance Show Type where
     show (TAtom  a)   = a
     show (TArrow a b) = "(" ++ show a ++ " -> " ++ show b ++ ")"
     show (TTuple a b) = "(" ++ show a ++ ", " ++ show b ++ ")"
-    show (TVar   v)   = show v
+    show (TVar v _)   = show v
     show (TSup   v)   = "v" ++ show v
     show (TInf   v)   = "^" ++ show v
     show (TLam i t)   = "\\" ++ show i ++ " " ++ show t
@@ -44,7 +44,7 @@ substituteType sub (TArrow a b)
 substituteType sub (TTuple a b) 
     = TTuple (substituteType sub a) (substituteType sub b)
 substituteType sub (TLam v t) 
-    = TLam v (substituteType (Map.delete (TVar v) sub) t)
+    = TLam v (substituteType (Map.delete (TVar v False) sub) t)
 substituteType _ x = x
 
 
@@ -70,7 +70,7 @@ allocateVar = do
 instantiateLam :: Type -> Compute Type
 instantiateLam (TLam v t) = do
     newvar <- allocateVar
-    return $ substituteType (Map.singleton (TVar v) (TVar newvar)) t
+    return $ substituteType (Map.singleton (TVar v False) (TVar newvar False)) t
 instantiateLam _ = error "Tried to lambda-instantiate a non-lambda"
 
 twoColumn :: Int -> String -> String -> String
@@ -89,15 +89,23 @@ addEquation reason eq@(Equation a b) = do
         else do
             depth <- ask
             liftIO $ putStrLn (twoColumn 50 (concat (replicate depth "  ") ++ show eq) ("(" ++ reason ++ ")"))
-            local (+1) $ transformEquation eq
             st <- get
             put (st { seenSet = Set.insert eq (seenSet st) })
+            local (+1) $ transformEquation eq
     
 
 -- x <: inf a  ok
 -- inf a <: x  inst
 -- sup a <: x  ok
 -- x <: sup a  inst
+
+generatedVar :: Type -> Bool
+generatedVar (TVar _ True) = True
+generatedVar _ = False
+    
+isInf :: Type -> Bool
+isInf (TInf _) = True
+isInf _ = False
 
 transformEquation :: Equation -> Compute ()
 transformEquation (Equation (TArrow a b) (TArrow a' b')) = do
@@ -108,47 +116,63 @@ transformEquation (Equation (TTuple a b) (TTuple a' b')) = do
     addEquation "tuple" (Equation a a')
     addEquation "tuple" (Equation b b')
 
-transformEquation (Equation sub@(TLam v t) sup) = do
+transformEquation (Equation sub@(TLam v t) sup) | not (isInf sup) = do
     sub' <- instantiateLam sub
     addEquation "instantiate" (Equation sub' sup)
 
-transformEquation (Equation (TInf a) b) = do
+transformEquation (Equation (TInf a) b) | not (generatedVar b) && not (isInf b) = do
     newvar <- allocateVar
-    addEquation "infiumum" (Equation (TInf a) (TVar newvar))
-    addEquation "infiumum" (Equation (TVar newvar) b)
+    addEquation "infiumum" (Equation (TInf a) (TVar newvar True))
+    addEquation "infiumum" (Equation (TVar newvar True) b)
     st <- get
     forEach (seenSet st) $ \eq ->
                 case eq of
                     Equation x (TInf y') | y' == a
-                        -> addEquation ("infiumum carry") (Equation x (TVar newvar))
+                        -> addEquation ("infiumum carry") (Equation x (TVar newvar True))
                     _   -> return ()
 
-transformEquation (Equation a (TSup b)) = do
+transformEquation (Equation a (TInf b)) = do
+    st <- get
+    forEach (seenSet st) $ \eq ->
+                case eq of
+                    Equation (TInf x') y | x' == b
+                        -> addEquation ("infimum carry") (Equation a y)
+                    _   -> return ()
+
+transformEquation (Equation a (TSup b)) | not (generatedVar a) = do
     newvar <- allocateVar
-    addEquation "supremum" (Equation (TVar newvar) (TSup b))
-    addEquation "supremum" (Equation a (TVar newvar))
+    addEquation "supremum" (Equation (TVar newvar True) (TSup b))
+    addEquation "supremum" (Equation a (TVar newvar True))
     st <- get
     forEach (seenSet st) $ \eq ->
                 case eq of
                     Equation (TSup x') y | x' == b
-                        -> addEquation ("supremum carry") (Equation (TVar newvar) y)
+                        -> addEquation ("supremum carry") (Equation (TVar newvar True) y)
+                    _   -> return ()
+
+transformEquation (Equation (TSup a) b) = do
+    st <- get
+    forEach (seenSet st) $ \eq ->
+                case eq of
+                    Equation x (TSup y') | y' == a
+                        -> addEquation ("supremum carry") (Equation x b)
                     _   -> return ()
 
 transformEquation (Equation sub sup) = do
     st <- get
     
     case sub of
-        TVar a' -> forEach (seenSet st) $ \eq -> 
+        TVar a' _ -> forEach (seenSet st) $ \eq -> 
                         case eq of
-                            Equation x (TVar y') | y' == a'
+                            Equation x (TVar y' _) | y' == a'
                                 -> addEquation ("left-elim  " ++ show sub) (Equation x sup)
                             _   -> return ()
         _ -> return ()
     
     case sup of
-        TVar b' -> forEach (seenSet st) $ \eq ->
+        TVar b' _ -> forEach (seenSet st) $ \eq ->
                         case eq of
-                            Equation (TVar x') y | x' == b'
+                            Equation (TVar x' _) y | x' == b'
                                 -> addEquation ("right-elim " ++ show sup) (Equation sub y)
                             _   -> return ()
         _ -> return ()
@@ -168,20 +192,23 @@ main = do
     putStrLn ""
     mapM_ print reduced
     where
-          --    a -> b        <:  \a (a -> a)
-    eqs = [ Equation (TArrow (TVar 0) (TVar 1)) (TLam 0 (TArrow (TVar 0) (TVar 0)))
-          --    \a (a -> a)   <:  a -> b
-          , Equation (TLam 0 (TArrow (TVar 0) (TVar 0))) (TArrow (TVar 0) (TVar 1))
+    tVar :: Integer -> Type
+    tVar i = TVar i False
+
+          --    a             <:  \a (a -> a)
+    eqs = [ Equation (TInf 0) (TLam 0 (TArrow (tVar 0) (tVar 0)))
+          --    \a (a -> a)   <:  a
+          , Equation (TLam 0 (TArrow (tVar 0) (tVar 0))) (TInf 0)
           -- .1 c             <:  d -> e
-          , Equation (TInf 2) (TArrow (TVar 3) (TVar 4))
+          , Equation (TInf 2) (TArrow (tVar 3) (tVar 4))
           --    Int           <:  d
-          , Equation (TAtom "Int") (TVar 3)
+          , Equation (TAtom "Int") (tVar 3)
           -- .2 c             <:  f -> g
-          , Equation (TInf 2) (TArrow (TVar 5) (TVar 6))
+          , Equation (TInf 2) (TArrow (tVar 5) (tVar 6))
           --    Str           <:  f
-          , Equation (TAtom "Str") (TVar 5)
+          , Equation (TAtom "Str") (tVar 5)
           -- *? c -> (e,g)    <:  h -> i
-          , Equation (TArrow (TInf 2) (TTuple (TVar 4) (TVar 6))) (TArrow (TVar 7) (TVar 8))
-          --    a -> b        <:  h
-          , Equation (TArrow (TVar 0) (TVar 1)) (TVar 7)
+          , Equation (TArrow (TInf 2) (TTuple (tVar 4) (tVar 6))) (TArrow (TInf 7) (tVar 8))
+          --    a             <:  h
+          , Equation (TInf 0) (TInf 7)
           ]

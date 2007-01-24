173dcf29e959bcd05acbc4b0b37f116c280d94ee
fibonaci <fibonaci>
2007-01-24 08:55:05 +0000

    Redid the equations in a way that a compiler would do them.

--- work/code/haskell/generalize.hs
+++ work/code/haskell/generalize.hs
@@ -36,11 +36,11 @@ type Subst = Map.Map Type Type
 type SubstID = Integer
 
 data Equation where
-    Equation :: Type -> Type -> Equation
+    (:<) :: Type -> Type -> Equation
     deriving (Eq,Ord)
 
 instance Show Equation where
-    show (Equation a b) = show a ++ " <: " ++ show b
+    show (a :< b) = show a ++ " <: " ++ show b
 
 
 substituteType :: Subst -> Type -> Type
@@ -89,7 +89,7 @@ twoColumn width cola colb
                  else 4
 
 addEquation :: String -> Equation -> Compute ()
-addEquation reason eq@(Equation a b) = do
+addEquation reason eq@(a :< b) = do
     st <- get
     if eq `Set.member` seenSet st
         then return ()
@@ -100,60 +100,40 @@ addEquation reason eq@(Equation a b) = do
             put (st { seenSet = Set.insert eq (seenSet st) })
             local (+1) $ (transformEquation eq >> performElimination eq)
     
-
--- x <: inf a  ok
--- inf a <: x  inst
--- sup a <: x  ok
--- x <: sup a  inst
-
--- a <: b may not be instantiated if both a and b are limit variables.
--- For example, if they were both infima then to instantiate would mean
--- to add the equations:
---   a <: a1
---   a1 <: b
--- That is, "there is some type a1 in a's defining set that is a subtype
--- of b".  But if a and b turned out to be the same, this would not be
--- true!  (The only supertype of a which is a subtype of b is a itself,
--- which is not singular!) 
---
--- This phenomenon of course happens when they are both suprema, but it
--- can even happen when one is a supremum and the other is an infimum.
--- Proof is left as an exercise for the reader. :-)
-    
 isLim :: Type -> Bool
 isLim (TLim _) = True
 isLim (TLam _ _) = True
 isLim _ = False
 
 transformEquation :: Equation -> Compute ()
-transformEquation (Equation (TArrow a b) (TArrow a' b')) = do
-    addEquation "arrow" (Equation a' a)
-    addEquation "arrow" (Equation b b')
+transformEquation (TArrow a b :< TArrow a' b') = do
+    addEquation "arrow" (a' :< a)
+    addEquation "arrow" (b :< b')
     
-transformEquation (Equation (TTuple a b) (TTuple a' b')) = do
-    addEquation "tuple" (Equation a a')
-    addEquation "tuple" (Equation b b')
+transformEquation (TTuple a b :< TTuple a' b') = do
+    addEquation "tuple" (a :< a')
+    addEquation "tuple" (b :< b')
 
-transformEquation (Equation sub@(TLam v t) sup) | not (isLim sup) = do
+transformEquation (sub@(TLam v t) :< sup) | not (isLim sup) = do
     sub' <- instantiateLam sub
-    addEquation "instantiate" (Equation sub' sup)
+    addEquation "instantiate" (sub' :< sup)
 
 transformEquation _ = return ()
 
 performElimination :: Equation -> Compute ()
-performElimination (Equation sub sup) = do
+performElimination (sub :< sup) = do
     st <- get
     
     forEach (seenSet st) $ \eq -> 
                 case eq of
-                    Equation x y | y == sub
-                        -> addEquation ("left-elim  " ++ show sub) (Equation x sup)
+                    x :< y | y == sub
+                        -> addEquation ("left-elim  " ++ show sub) (x :< sup)
                     _   -> return ()
     
     forEach (seenSet st) $ \eq ->
                 case eq of
-                    Equation x y | x == sup
-                        -> addEquation ("right-elim " ++ show sup) (Equation sub y)
+                    x :< y | x == sup
+                        -> addEquation ("right-elim " ++ show sup) (sub :< y)
                     _   -> return ()
 
 
@@ -165,30 +145,17 @@ reduce start eqs = fmap (Set.toList . seenSet)
                                 , seenSet       = Set.empty
                                 }
 
-(<:) :: Type -> Type -> Equation
-(<:) = Equation
-
 main :: IO ()
 main = do
     reduced <- reduce 100 eqs
     putStrLn ""
     mapM_ print reduced
     where
-          --    a             <:  \a (a -> a)
-    eqs = [ Equation (TLim 0) (TLam 0 (TArrow (TVar 0) (TVar 0)))
-          --    \a (a -> a)   <:  a
-          , Equation (TLam 0 (TArrow (TVar 0) (TVar 0))) (TLim 0)
-          -- .1 c             <:  d -> e
-          , Equation (TLim 2) (TArrow (TLim 3) (TLim 4))
-          --    Int           <:  d
-          , Equation (TAtom "Int") (TLim 3)
-          -- .2 c             <:  f -> g
-          , Equation (TLim 2) (TArrow (TLim 5) (TLim 6))
-          --    Str           <:  f
-          , Equation (TAtom "Str") (TLim 5)
-          -- *? c -> (e,g)    <:  h -> i
-          , Equation (TArrow (TLim 2) (TTuple (TLim 4) (TLim 6))) (TArrow (TLim 7) (TLim 8))
-          --    a             <:  h
-          , Equation (TLim 0) (TLim 7)
+    eqs = [ TLim 4                            :< TArrow (TLim 0) (TLim 3)
+          , TLam 0 (TArrow (TVar 0) (TVar 0)) :< TLim 0
+          , TTuple (TLim 1) (TLim 2)          :< TLim 3
+          , TLim 0                            :< TArrow (TLim 5) (TLim 1)
+          , TAtom "Int"                       :< TLim 5
+          , TLim 0                            :< TArrow (TLim 6) (TLim 2)
+          , TAtom "Str"                       :< TLim 6
           ]
-

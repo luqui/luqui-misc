2f0fbf431aaecaa6e95047d4bdbcfc818f6630ea
fibonaci <fibonaci>
2007-01-29 03:41:21 +0000

    Added an analysis barfer.

---
+++ work/class/PHYS_4810/final_project/survey/analyze.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use IO::All;
+
+my %ans;
+
+for my $file (io('data/')->all) {
+    my $survey = do $file;
+    for (keys %$survey) {
+        next if /SEED|studentid|fullname/;
+        unless (/explain/) {
+            my $answer = $survey->{$_};
+            if (ref $answer) {
+                $answer = join ' ', map { "$_($answer->{$_})" } sort keys %$answer;
+            }
+            $ans{$_}{$answer}{count}++;
+            if ($survey->{"$_\_explain"}) {
+                push @{$ans{$_}{$answer}{explain}}, $survey->{"$_\_explain"};
+            }
+        }
+    }
+}
+
+for my $question (sort keys %ans) {
+    print "$question:\n";
+    for my $answer (sort { $ans{$question}{$b}{count} <=> $ans{$question}{$a}{count} } keys %{$ans{$question}}) {
+        print "  $ans{$question}{$answer}{count} - $answer\n";
+        for my $explain (@{$ans{$question}{$answer}{explain}}) {
+            print "    $explain\n";
+        }
+    }
+}

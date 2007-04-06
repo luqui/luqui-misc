6453af89e01ba26e45588be1a5f623a4a664b1aa
fibonaci <fibonaci>
2007-04-06 01:42:23 +0000

    Drawing a crappy looking mandel now.

--- work/code/c/mandelgts/main.cc
+++ work/code/c/mandelgts/main.cc
@@ -5,15 +5,63 @@
 #include <cmath>
 #include <iostream>
 
-void circleFunc(gdouble** a, GtsCartesianGrid g, guint i, gpointer data)
+struct Quaternion
+{
+    Quaternion()
+        : r(0), i(0), j(0), k(0)
+    { }
+
+    Quaternion(gdouble r, gdouble i, gdouble j, gdouble k)
+        : r(r), i(i), j(j), k(k)
+    { }
+    
+    gdouble r,i,j,k;
+    
+    gdouble norm2() const {
+        return r*r + i*i + j*j + k*k;
+    }
+};
+
+Quaternion operator * (const Quaternion& a, const Quaternion& b)
+{
+    return Quaternion
+                ( a.r * b.r - a.i * b.i - a.j * b.j - a.k * b.k
+                , a.r * b.i + a.i * b.r + a.j * b.k - a.k * b.j
+                , a.r * b.j - a.i * b.k + a.j * b.r + a.k * b.i
+                , a.r * b.k + a.i * b.j - a.j * b.i + a.k * b.r);
+}
+
+Quaternion operator + (const Quaternion& a, const Quaternion& b)
+{
+    return Quaternion(a.r + b.r, a.i + b.i, a.j + b.j, a.k + b.k);
+}
+
+double quaternibrot(const Quaternion& c, int bailout) {
+    Quaternion z;
+    int iters = 0;
+    while (true) {
+        z = z*z + c;
+        gdouble norm = z.norm2();
+        if (norm > 4) {
+            return 1.0/iters - 1.0/bailout;
+        }
+        if (iters++ > bailout) {
+            return norm-4;
+        }
+    }
+    
+}
+
+void quaternibrotFunc(gdouble** a, GtsCartesianGrid g, guint i, gpointer data)
 {
     for (int x = 0; x < g.nx; x++) {
         gdouble fx = g.x + g.dx * x;
         for (int y = 0; y < g.ny; y++) {
             gdouble fy = g.y + g.dy * y;
-            a[x][y] = fx*fx + fy*fy + g.z*g.z - 4;
+            a[x][y] = quaternibrot(Quaternion(fx, fy, g.z, 0), 50);
         }
     }
+    std::cout << "\r" << gdouble(i)/g.nz << std::flush;
 }
 
 void drawFace(GtsFace* face)
@@ -48,14 +96,14 @@ void drawSurface(GtsSurface* surf)
 }
 
 
-GtsSurface* makeSphere()
+GtsSurface* makeSurface()
 {
     GtsSurface* surf = gts_surface_new(gts_surface_class(), gts_face_class(), gts_edge_class(), gts_vertex_class());
     GtsCartesianGrid grid;
-    grid.nx = grid.ny = grid.nz = 50;
-    grid.x = grid.y = grid.z = -4.0;
-    grid.dx = grid.dy = grid.dz = 8.0/50;
-    gts_isosurface_cartesian(surf, grid, &circleFunc, NULL, 0);
+    grid.nx = grid.ny = grid.nz = 100;
+    grid.x = grid.y = grid.z = -2.0;
+    grid.dx = grid.dy = grid.dz = 4.0/100;
+    gts_isosurface_cartesian(surf, grid, &quaternibrotFunc, NULL, 0);
     return surf;
 }
 
@@ -83,7 +131,7 @@ void init()
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0, 4.0/3.0, 0.1, 100.0);
-    gluLookAt(0, -8, 0, 0, 0, 0, 0, 0, 1);
+    gluLookAt(0, -4, 0, 0, 0, 0, 0, 0, 1);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 }
@@ -93,11 +141,16 @@ int main()
     init();
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    GtsSurface* sphere = makeSphere();
-    drawSurface(sphere);
-    std::cout << "Drew " << FACES << " faces\n";
+    GtsSurface* surf = makeSurface();
+    drawSurface(surf);
+    std::cout << "\nDrew " << FACES << " faces\n";
     SDL_GL_SwapBuffers();
 
-    SDL_Delay(2000);
+    SDL_Event e;
+    while (SDL_WaitEvent(&e)) {
+        if (e.type == SDL_KEYDOWN) {
+            break;
+        }
+    }
     SDL_Quit();
 }

8ba28bf011b00935c113be253819fdba72cc390c
fibonaci <fibonaci>
2007-05-11 09:46:34 +0000

    ?

--- work/code/haskell/selfref.hs
+++ work/code/haskell/selfref.hs
@@ -7,6 +7,7 @@ import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
 
 type OcMap = Map.Map Int Int
+type Range = [Int]
 
 countOccurrences :: OcMap -> OcMap
 countOccurrences = 
@@ -14,15 +15,30 @@ countOccurrences =
         (\k v -> insertOn k . insertOn v)
         Map.empty
     where
+    -- separate into digits
     insertOn :: Int -> OcMap -> OcMap
     insertOn = foldr (\x -> (Map.insertWith (+) (read [x]) 1 .)) id . show
 
-prune :: OcMap -> OcMap
-prune m = Map.intersection m $ Map.fromList [ (n,undefined) | n <- [0..9] ]
+prune :: Range -> OcMap -> OcMap
+prune r m = Map.intersection m $ Map.fromList [ (n,undefined) | n <- r ]
 
-fap :: Int -> (a -> a) -> (a -> a)
-fap 0 _ = id
-fap n f = f . fap (n-1) f
+fixPoint :: (Eq a) => (a -> a) -> a -> a
+fixPoint f a = 
+    let next = f a in
+        if next == a 
+            then a 
+            else fixPoint f next
 
-iter :: Int -> OcMap
-iter n = fap n (prune . countOccurrences) $ Map.fromList [ (n,0) | n <- [0..9] ]
+fixPointMax :: (Eq a) => Int -> (a -> a) -> a -> Maybe a
+fixPointMax 0 _ _ = Nothing
+fixPointMax n f a =
+    let next = f a in
+        if next == a
+            then Just a
+            else fixPointMax (n-1) f next
+
+solve :: Range -> OcMap
+solve r = fixPoint (prune r . countOccurrences) $ Map.fromList [ (n,0) | n <- r ]
+
+solveMax :: Int -> Range -> Maybe OcMap
+solveMax n r = fixPointMax n (prune r . countOccurrences) $ Map.fromList [ (n,0) | n <- r ]

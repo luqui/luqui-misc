009a101bc2d5d025e03f6045edba987682c51c8b
fibonaci <fibonaci>
2007-05-24 07:52:12 +0000

    Genericified the generator.

--- work/code/haskell/polyselfgen.hs
+++ work/code/haskell/polyselfgen.hs
@@ -1,11 +1,11 @@
 import Data.Ratio
 
-nextseq :: [Rational] -> Rational
+nextseq :: (Num a) => [a] -> a
 nextseq rs = 
     let x = last rs in
         sum $ zipWith (\a b -> a * x^b) rs [0..]
 
-genseq :: Rational -> [Rational]
+genseq :: (Num a) => a -> [a]
 genseq r = genseq'
     where
     genseq' = map head $ iterate (\as -> nextseq as : as) [r]

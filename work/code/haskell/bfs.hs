4f1b4e787b13ab453dd370eacd69aa80642526d9
fibonaci <fibonaci>
2007-07-23 23:14:23 +0000

    Made bfs prettier.

--- work/code/haskell/bfs.hs
+++ work/code/haskell/bfs.hs
@@ -1,8 +1,17 @@
 data Tree a = Branch { value :: a, children :: [Tree a] }
 
+ap :: (a -> b -> c) -> (a -> b) -> a -> c
+ap f g x = f x (g x)
+
 -- Breadth-first search
 bfs :: Tree a -> [a]
-bfs tree = bfs' [tree]
+bfs tree = map value $ bfs' [tree]
 	where
-	bfs' [] = []
-	bfs' trees = map value trees ++ (bfs' (concatMap children trees))
+        bfs' [] = []
+        bfs' xs = ap (++) (bfs' . concatMap children) xs
+
+-- Duh-first search
+dfs :: Tree a -> [a]
+dfs tree = map value $ dfs' [tree]
+        where
+        dfs' xs = concatMap (ap (:) (dfs' . children)) xs

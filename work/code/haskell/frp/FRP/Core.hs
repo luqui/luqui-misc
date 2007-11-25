75e7cbdcc7bbdcb88140d63a7eb4eda27a3101f4
fibonaci <fibonaci>
2007-11-25 21:25:26 +0000

    Holy crap it's working.

--- work/code/haskell/frp/FRP/Core.hs
+++ work/code/haskell/frp/FRP/Core.hs
@@ -8,8 +8,6 @@ module FRP.Core
     )
 where
 
-import qualified Graphics.UI.SDL as SDL
-
 class (Functor w) => Comonad w where
     pull   :: w a -> a
     cojoin :: w a -> w (w a)
@@ -19,19 +17,23 @@ class (Functor w) => Comonad w where
     cojoin w = w =>> id
 
 type Time = Double
-type ExtEvent = SDL.Event
+type ExtEvent = ()
+
+data DriveEvent 
+    = TimeStepEvent
+    | ExtEvent ExtEvent
+
+type Driver = (Time,DriveEvent)
 
 data Behavior a
     = Behavior { bEval  :: a
-               , bTrans :: (ExtEvent -> Behavior a)
-               , bTime  :: Time
+               , bTrans :: (Driver -> Behavior a)
                }
 
 instance Functor Behavior where
-    fmap f (Behavior eval trans t) = 
+    fmap f (Behavior eval trans) = 
         Behavior { bEval  = f eval
                  , bTrans = fmap f . trans
-                 , bTime  = t
                  }
 
 instance Comonad Behavior where
@@ -39,46 +41,58 @@ instance Comonad Behavior where
     b =>> f =
         Behavior { bEval  = f b
                  , bTrans = \e -> bTrans b e =>> f
-                 , bTime  = bTime b
                  }
+
+constB :: a -> Behavior a
+constB x = let r = Behavior { bEval = x, bTrans = const r } in r
+
+zipB :: Behavior a -> Behavior b -> Behavior (a,b)
+zipB b b' = tup (b, b')
+    where
+    tup (t,t') = Behavior { bEval = (bEval t, bEval t')
+                          , bTrans = \e -> tup (bTrans t e, bTrans t' e)
+                          }
 
 data Event :: * -> * where
     EVNever    :: Event a
-    EVConst    :: Time -> a -> Event a
-    EVExt      :: (ExtEvent -> Maybe (Event a)) -> Event a
-    EVPred     :: Behavior (Maybe (Time,a)) -> Event a
+    EVDriver   :: Behavior (Maybe a) -> Event a
     EVChoice   :: Event a -> Event a -> Event a
     EVSnapshot :: Event a -> Behavior b -> Event (a,b)
     EVJoin     :: Event (Event a) -> Event a
 
-infixl 2 +=>
-
-(+=>) :: Event a -> (Time -> a -> b) -> Event b
-EVNever        +=> _ = EVNever
-EVConst t x    +=> f = EVConst t (f t x)
-EVExt f'       +=> f = EVExt (fmap (+=> f) . f')
-EVPred b       +=> f = EVPred (fmap (fmap (\(t,a) -> (t,f t a))) b)
-EVChoice x y   +=> f = EVChoice (x +=> f) (y +=> f)
-EVSnapshot e b +=> f = e +=> (\t x -> f t (x, pull b))
-EVJoin e       +=> f = EVJoin (e +=> (\_ x -> x +=> f))
-
 instance Functor Event where
-    fmap f x = x +=> const f
-    {-
-    fmap _ EVNever = EVNever
-    fmap f (EVConst t x) = EVConst t (f x)
-    fmap f (EVExt f') = EVExt (\e -> fmap (fmap f) (f' e))
-    fmap f (EVPred b) = EVPred (fmap (fmap f) b)
-    fmap f (EVChoice x y) = EVChoice (fmap f x) (fmap f y)
+    fmap _ EVNever          = EVNever
+    fmap f (EVDriver b)     = EVDriver (fmap (fmap f) b)
+    fmap f (EVChoice x y)   = EVChoice (fmap f x) (fmap f y)
     fmap f (EVSnapshot e b) = fmap (\x -> f (x, pull b)) e
-    fmap f (EVJoin e) = EVJoin (fmap (fmap f) e)
-    -}
+    fmap f (EVJoin e)       = EVJoin (fmap (fmap f) e)
 
 untilB :: Behavior a -> Event (Behavior a) -> Behavior a
-untilB b (EVNever) = b
-untilB b (EVConst t a) = if bTime b < t then b else a
-untilB b (EVExt f) = b { bTrans = maybe b (untilB b) . f }
-untilB b (EVPred b') = maybe b snd (pull b')
+untilB b (EVNever)      = b
+untilB b (EVDriver b')  = 
+    case pull b' of
+         Nothing -> 
+            Behavior { bEval  = pull b
+                     , bTrans = \e -> untilB (bTrans b e) (EVDriver (bTrans b' e))
+                     }
+         Just x -> x
 untilB b (EVChoice x y) = untilB (untilB b y) x
--- EVSnapshot impossible: (a,b) /~ Behavior a
-untilB b (EVJoin e) = untilB b (fmap (untilB b) e)
+untilB b (EVJoin e)     = untilB b (fmap (untilB b) e)
+-- EVSnapshot impossible, (a,b) /~ Behavior c
+
+time :: Behavior Double
+time = genB 0
+    where
+    genB t = Behavior { bEval = t
+                      , bTrans = \(t',_) -> genB (t+t')
+                      }
+
+step :: Double -> Behavior a -> Behavior a
+step size b = bTrans b (size,TimeStepEvent)
+
+delay :: Double -> Behavior a -> Event a
+delay offs b = EVDriver $ 
+    zipB time b =>> \w -> 
+        if fst (pull w) >= offs
+           then Just (snd (pull w))
+           else Nothing

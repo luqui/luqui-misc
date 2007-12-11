81001ca8a01d3eb83b2b5d070a20209b2d67c4e3
fibonaci <fibonaci>
2007-12-11 04:34:53 +0000

    Remove differentiate from the differentiable interface (!).

--- work/code/haskell/frp/Fregl/Differentiable.hs
+++ work/code/haskell/frp/Fregl/Differentiable.hs
@@ -4,10 +4,8 @@ module Fregl.Differentiable where
 
 class Differentiable d where
     type Derivative d :: *
-    differentiate :: Double -> d -> d -> Derivative d
     integrate     :: Double -> Derivative d -> d -> d
 
 instance Differentiable Double where
     type Derivative Double = Double
-    differentiate dt a b = (b - a) / dt
     integrate     dt d a = a + d*dt

a20a99b9a5cb2ef2d3e82f2be01ccfd81de0fac6
fibonaci <fibonaci>
2008-01-05 15:03:46 +0000

    oh... my... god.
    Comonadic FRP With Value Recursion!!!!

---
+++ work/code/haskell/signalbuf.hs
@@ -0,0 +1,27 @@
+import Debug.Trace
+
+type Time = Double
+data Signal a 
+    = Signal a (Time -> Signal a)
+
+instance Functor Signal where
+    fmap f ~(Signal x cont) = Signal (f x) (fmap f . cont)
+
+integral :: Double -> Signal Double -> Signal Double
+integral init ~(Signal x cont) = 
+    Signal init (\dt -> integral (trace "add" $ init + dt*x) $ cont dt)
+
+runSignal :: Double -> Signal a -> [a]
+runSignal step (Signal x cont) = x : runSignal step (cont step)
+
+listSignal :: Double -> [a] -> Signal a
+listSignal step = listSignal' 0
+    where
+    listSignal' offs list@(x:xs)
+        | offs < 0 = listSignal' (offs + step) xs
+        | otherwise =
+            Signal x $ \dt -> listSignal' (offs-dt) list
+
+buffer :: Double -> Signal a -> Signal a
+buffer step = listSignal step . runSignal step
+

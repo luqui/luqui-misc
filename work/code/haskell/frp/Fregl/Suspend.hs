1a804e6a7746d41bdca0b34ab8342e53449cd921
fibonaci <fibonaci>
2008-01-08 10:52:03 +0000

    Added frp again, and implemented the SuspendT monad transformer, the basis for the Event monad.

---
+++ work/code/haskell/frp/Fregl/Suspend.hs
@@ -0,0 +1,31 @@
+module Fregl.Suspend 
+    ( MonadSuspend(..)
+    , SuspendT, runSuspendT
+    )
+where
+
+import Control.Monad
+import Control.Monad.Trans
+
+class (Monad m) => MonadSuspend v m where
+    attempt :: m a -> m (Either a (v -> m a))
+    suspend :: m v
+
+newtype SuspendT v m a 
+    = SuspendT { runSuspendT :: m (Either a (v -> SuspendT v m a)) }
+
+instance (Monad m) => Monad (SuspendT v m) where
+    return x = SuspendT $ return $ Left x
+    m >>= k  = SuspendT $ do
+        choice <- runSuspendT m
+        case choice of
+             Left a  -> runSuspendT $ k a
+             Right cont -> return $ Right $ \v -> cont v >>= k
+
+
+instance MonadTrans (SuspendT v) where
+    lift = SuspendT . liftM Left
+
+instance (Monad m) => MonadSuspend v (SuspendT v m) where
+    attempt = lift . runSuspendT
+    suspend = SuspendT $ return $ Right return

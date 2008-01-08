309c39d85b2d4cd7d753c3cb9a488e968d6f8b7e
fibonaci <fibonaci>
2008-01-08 13:22:24 +0000

    newEventCxt

--- work/code/haskell/frp/Fregl/Event.hs
+++ work/code/haskell/frp/Fregl/Event.hs
@@ -5,9 +5,12 @@ import Fregl.Signal
 import Control.Monad.Writer
 import Control.Concurrent.STM
 import Control.Applicative
+import Data.Monoid
+
+type ContLog v = Endo [v -> Event v ()]
 
 newtype Event v a
-    = Event { runEvent :: SuspendT v (WriterT [v -> Event v ()] IO) a }
+    = Event { runEvent :: SuspendT v (WriterT (ContLog v) IO) a }
 
 newtype Behavior v a = Behavior { bindBehavior :: Event v (Signal a) }
 
@@ -56,7 +59,28 @@ untilEvent b ev = Behavior $ Event $ do
          Right cont -> do
              sigb <- runEvent $ bindBehavior b
              cell <- liftIO $ atomically $ newSignalCell sigb
-             lift $ tell $ (:[]) $ \v -> Event $ do
+             lift $ tell $ Endo $ (:) $ \v -> Event $ do
                  sig <- cont v
                  liftIO $ atomically $ overwriteSignalCell cell sig
              return $ cellSignal cell
+
+-- executing events:
+data EventCxt v a = EventCxt a (v -> IO (EventCxt v a))
+
+newEventCxt :: forall v a. Behavior v a -> IO (EventCxt v a)
+newEventCxt b = do
+    let event = bindBehavior b
+    let suspend = runEvent event
+    (Left sig, conts) <- runWriterT (runSuspendT suspend)
+    val <- atomically $ readSignal sig
+    return $ EventCxt val (contAction sig conts)
+    where
+    contAction :: Signal a -> ContLog v -> v -> IO (EventCxt v a)
+    contAction sig conts v = do
+        conts' <- forM (appEndo conts []) $ \cont -> do
+            (choice, cs) <- runWriterT $ runSuspendT $ runEvent $ cont v
+            case choice of
+                 Left () -> return cs
+                 Right cont -> return $ cs `mappend` (Endo $ (:) (Event . cont))
+        val <- atomically $ readSignal sig
+        return $ EventCxt val (contAction sig (mconcat conts'))

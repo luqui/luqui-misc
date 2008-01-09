a1468bb82ffa4a417e064b544a8b32154721b077
fibonaci <fibonaci>
2008-01-09 05:53:11 +0000

    Vector class.

---
+++ work/code/haskell/frp/Fregl/Vector.hs
@@ -0,0 +1,47 @@
+{-# OPTIONS_GHC -fglasgow-exts #-}
+
+module Fregl.Vector where
+
+class (Num (Field v)) => Vector v where
+    type Field v :: *
+    vzero :: v
+    (^+^) :: v -> v -> v
+    (*^)  :: Field v -> v -> v
+
+class (Vector v) => InnerProduct v where
+    (^*^) :: v -> v -> Field v
+
+infixl 6 ^+^, ^-^
+infixl 7 *^, ^*, ^*^, ^/
+
+(^-^) :: (Vector v) => v -> v -> v
+u ^-^ v = u ^+^ (-1) *^ v
+
+(^*) :: (Vector v) => v -> Field v -> v
+(^*) = flip (*^)
+
+(^/) :: (Vector v, Fractional (Field v)) => v -> Field v -> v
+v ^/ a = recip a *^ v
+
+norm2 :: (InnerProduct v) => v -> Field v
+norm2 v = v ^*^ v
+
+norm :: (InnerProduct v, Floating (Field v)) => v -> Field v
+norm = sqrt . norm2
+
+normalize :: (InnerProduct v, Floating (Field v)) => v -> v
+normalize v = v ^/ norm v
+
+
+type Euclid2 a = (a,a)
+type Vec2 = Euclid2 Double
+
+instance (Num a) => Vector (Euclid2 a) where
+    type Field (Euclid2 a) = a
+    vzero = (0,0)
+    (x,y) ^+^ (x',y') = (x+x', y+y')
+    a *^ (x,y) = (a*x, a*y)
+
+instance (Num a) => InnerProduct (Euclid2 a) where
+    (x,y) ^*^ (x',y') = x*x' + y*y'
+

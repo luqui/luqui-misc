7397c08087d712d6610f5d2983ea0ffeef4f2529
fibonaci <fibonaci>
2008-01-13 17:58:51 +0000

    Forgot to add my image.

--- work/code/haskell/frp/examples/sprite.hs
+++ work/code/haskell/frp/examples/sprite.hs
@@ -4,5 +4,5 @@ import Fregl.Event
 
 main = do
     runGameSDL $ \_ -> do
-        stick <- unsafeEventIO $ Draw.imageToSprite "turquoise_stick.png"
+        stick <- unsafeEventIO $ Draw.imageToSprite "res/turquoise_stick.png"
         return $ pure $ Draw.sprite stick

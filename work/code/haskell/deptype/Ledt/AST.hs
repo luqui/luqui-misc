b886186b5341edb30d201cebca7f375bd91abfe7
fibonaci <fibonaci>
2008-02-22 09:49:40 +0000

    new exp type with pretty printer (destroyed typechecker).

--- work/code/haskell/deptype/Ledt/AST.hs
+++ work/code/haskell/deptype/Ledt/AST.hs
@@ -1,47 +1,17 @@
-module Ledt.AST where
+module Ledt.AST 
+    ( AST(..)
+    , ASTInfer(..) ) 
+where
 
 import Prelude hiding (pi)
 
-type Var = String
-
+infixl 0 %
 class AST ast where
-    -- lambda calculus:
-    var :: Var -> ast                -- x
-    lam :: Var -> ast -> ast -> ast  -- \x:e₁. e₂
-    app :: ast -> ast -> ast         -- e₁ e₂
+    lam  :: ast -> (ast -> ast) -> ast
+    pi   :: ast -> (ast -> ast) -> ast
+    (%)  :: ast -> ast -> ast
+    set  :: Integer -> ast
 
-    -- types
-    pi  :: Var -> ast -> ast -> ast  -- (x:e₁ -> e₂)
-    set :: Integer    -> ast         -- Set n
-
-
-data Exp 
-    = EVar Var
-    | ELam Var Exp Exp
-    | EPi  Var Exp Exp
-    | EApp Exp Exp 
-    | ESet Integer
-
-instance Show Exp where
-    show (EVar v) = v
-    show (ELam v t b) = "(\\" ++ v ++ ":" ++ show t ++ ". " ++ show b ++ ")"
-    show (EApp e e') = "(" ++ show e ++ " " ++ show e' ++ ")"
-    show (EPi v t b)
-        | free v b  = "(" ++ show t ++ " -> " ++ show b ++ ")"
-        | otherwise = "(*" ++ v ++ ":" ++ show t ++ ". " ++ show b ++ ")"
-    show (ESet 0) = "Set"
-    show (ESet i) = "Set" ++ show i
-
-free :: Var -> Exp -> Bool
-free v (EVar v') = v /= v'
-free v (ELam v' t b) = free v t && (v == v' || free v b)
-free v (EPi v' t b) = free v t && (v == v' || free v b)
-free v (EApp e e') = free v e && free v e'
-free v (ESet i) = True
-
-instance AST Exp where
-    var = EVar
-    lam = ELam
-    app = EApp
-    set = ESet
-    pi  = EPi
+class (AST ast) => ASTInfer ast where
+    lam_ :: (ast -> ast) -> ast
+    pi_  :: (ast -> ast) -> ast

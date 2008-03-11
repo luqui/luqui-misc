c6cf077963dbdd8ee1754f0efa53cd41a8bb6f34
fibonaci <fibonaci>
2008-03-11 08:14:13 +0000

    Updated example code.

--- work/code/haskell/modules/quantum-arrow/example.hs
+++ work/code/haskell/modules/quantum-arrow/example.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -fglasgow-exts -farrows #-}
 
-import Quantum
 import Control.Arrow
+import Control.Arrow.Quantum
 import Data.Complex
 
 
@@ -12,18 +12,24 @@ makeStates = proc () ->
                 ,( 3,   0  :+ 1)
                 ]
 
--- run these like: execQuantum detangle ()
+-- Run these example procedures like: execQuantum detangle ()
 
+-- This will output 4, -4, and 3 with equal probability.
 detangle :: Quantum () ()
 detangle = proc () -> do
     x <- makeStates -< ()
     qIO print -< x
 
+-- This will only ever output 3, because 4^2 == (-4)^2 and
+-- their probability amplitudes (1 and -1, resp.) cancel each
+-- other out.
 detangle2 :: Quantum () ()
 detangle2 = proc () -> do
     x <- makeStates -< ()
     qIO print -< x^2
 
+-- This is an example from the Quantum::Entanglement perl module
+-- documentation that inspired this module.
 perlExample :: Quantum () ()
 perlExample = proc () -> do
     c <- entangle -< [(0, 1 :+ 0), (1, 0 :+ 1)] -- c = |0> + i|1>
@@ -33,7 +39,9 @@ perlExample = proc () -> do
         then qIO_ (putStrLn "e = 1") -< ()
         else qIO_ (putStrLn "e = 0") -< ()
     qIO putStrLn -< "(c,d) = " ++ show (c,d)
-    
+
+-- This demonstrates that conditionals do *not* collapse the state
+-- space, unless they must (for example if I/O is done within a branch)
 nonCollapsingConditional :: Quantum () ()
 nonCollapsingConditional = proc () -> do
     x <- entangle -< [(1, 1 :+ 0), (2, (-1) :+ 0), (3, 0 :+ 1)]

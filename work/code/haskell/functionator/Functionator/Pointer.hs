5818bdddff6bb567f9ddcaaeb4ceef72b11b563a
Luke Palmer <lrpalmer@gmail.com>
2008-03-24 23:16:19 +0000

    Wrote unify function.

--- work/code/haskell/functionator/Functionator/Pointer.hs
+++ work/code/haskell/functionator/Functionator/Pointer.hs
@@ -21,3 +21,16 @@ freeSubstitutePointer s i t p =
     where
     s1:s2:s3:s4:_ = split s
     accumFunc s v = (supplyLeft s, freeSubstitute (supplyRight s) i t v)
+
+
+unify :: Supply Int -> Type -> Type -> Pointer -> Pointer
+unify s (TFree i) (TFree j) p | i == j = p
+unify s (TFree i) t p | not (freeOccurs i t) = freeSubstitutePointer s i t p
+unify s t (TFree i) p | not (freeOccurs i t) = freeSubstitutePointer s i t p
+unify s (TPi v body) t p = 
+    let fv = TFree (supplyValue s)
+    in unify (supplyLeft s) (varSubstitute (supplyRight s) v fv body) t p
+unify s t t'@(TPi {}) p = unify s t' t p
+unify s (TVar v) (TVar v') p | v == v'   = p
+unify s (TApp a b) (TApp a' b') p = unify (supplyLeft s) a a' $ unify (supplyRight s) b b' p
+unify _ a b _ = error $ "Can't unify " ++ show a ++ " with " ++ show b

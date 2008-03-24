45269203aa3014f59a7af5fe6026668198796b3f
Luke Palmer <lrpalmer@gmail.com>
2008-03-24 23:30:01 +0000

    Wrote the important stuff in Pointer, presumably.

--- work/code/haskell/functionator/Functionator/Pointer.hs
+++ work/code/haskell/functionator/Functionator/Pointer.hs
@@ -8,7 +8,6 @@ data Pointer
     = Pointer { pEnv :: Map.Map Var Type
               , pCxt :: ExpCxt
               , pExp :: Exp
-              , pType :: Type
               }
 
 freeSubstitutePointer :: Supply Int -> Int -> Type -> Pointer -> Pointer
@@ -16,10 +15,9 @@ freeSubstitutePointer s i t p =
     Pointer { pEnv  = snd $ Map.mapAccum accumFunc s1 (pEnv p)
             , pCxt  = freeSubstituteExpCxt s2 i t (pCxt p)
             , pExp  = freeSubstituteExp s3 i t (pExp p)
-            , pType = freeSubstitute s4 i t (pType p)
             }
     where
-    s1:s2:s3:s4:_ = split s
+    s1:s2:s3:_ = split s
     accumFunc s v = (supplyLeft s, freeSubstitute (supplyRight s) i t v)
 
 
@@ -32,5 +30,34 @@ unify s (TPi v body) t p =
     in unify (supplyLeft s) (varSubstitute (supplyRight s) v fv body) t p
 unify s t t'@(TPi {}) p = unify s t' t p
 unify s (TVar v) (TVar v') p | v == v'   = p
-unify s (TApp a b) (TApp a' b') p = unify (supplyLeft s) a a' $ unify (supplyRight s) b b' p
+unify s (TApp a b) (TApp a' b') p = 
+    unify (supplyLeft s) a a' $ unify (supplyRight s) b b' p
 unify _ a b _ = error $ "Can't unify " ++ show a ++ " with " ++ show b
+
+
+makeApp :: Supply Int -> Pointer -> Pointer
+makeApp s p
+    | EHole resultType <- pExp p =
+        let s1:s2:_ = split s
+            t1 = TFree (supplyValue s1)
+            t2 = TFree (supplyValue s2)
+        in p { pExp = EApp (EHole (makeArrow t1 resultType)) (EHole t2) }
+    | otherwise = error $ "Expression not empty: " ++ show (pExp p)
+
+makeLambda :: Supply Int -> Var -> Pointer -> Pointer
+makeLambda s v p
+    | EHole resultType <- pExp p =
+        let s1:s2:s3:_ = split s
+            t1 = TFree (supplyValue s1)
+            t2 = TFree (supplyValue s2)
+        in unify s3 (makeArrow t1 t2) resultType $ 
+                p { pExp = ELambda v t1 (EHole t2) }
+    | otherwise = error $ "Expression not empty: " ++ show (pExp p)
+
+makeVar :: Supply Int -> Var -> Pointer -> Pointer
+makeVar s v p
+    | Just varType <- Map.lookup v (pEnv p)
+    , EHole resultType <- pExp p =
+        unify s varType resultType $ p { pExp = EVar v }
+    | otherwise = error $ "Expression not empty (" ++ show (pExp p)
+                       ++ ") or variable (" ++ show v ++ ") not in environment"

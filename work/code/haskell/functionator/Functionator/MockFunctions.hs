73a28c155d6e2c62fbc2b5e8bdcb2ba1ebbc85b0
Luke Palmer <lrpalmer@gmail.com>
2008-03-24 23:57:19 +0000

    Added a file full of mock functions.

---
+++ work/code/haskell/functionator/Functionator/MockFunctions.hs
@@ -0,0 +1,28 @@
+module Functionator.MockFunctions where
+
+import Functionator.AST
+import qualified Data.Map as Map
+
+
+mockEnv :: Map.Map Var Type
+mockEnv = Map.fromList
+    [ "(+)"     .:: TVar "Int" --> TVar "Int" --> TVar "Int"
+    , "(-)"     .:: TVar "Int" --> TVar "Int" --> TVar "Int"
+    , "(*)"     .:: TVar "Int" --> TVar "Int" --> TVar "Int"
+    , "even"    .:: TVar "Int" --> TVar "Bool"
+    , "odd"     .:: TVar "Int" --> TVar "Bool"
+    , "if"      .:: TPi "a" (TVar "Bool" --> TVar "a" --> TVar "a" --> TVar "a")
+    , "not"     .:: TVar "Bool" --> TVar "Bool"
+    , "head"    .:: TPi "a" (TVar "[]" %% TVar "a" --> TVar "a")
+    , "tail"    .:: TPi "a" (TVar "[]" %% TVar "a" --> TVar "[]" %% TVar "a")
+    , "cons"    .:: TPi "a" (TVar "a" --> TVar "[]" %% TVar "a" --> TVar "[]" %% TVar "a")
+    , "nil"     .:: TPi "a" (TVar "[]" %% TVar "a")
+    , "map"     .:: TPi "a" (TPi "b" ((TVar "a" --> TVar "b") --> TVar "[]" %% TVar "a" --> TVar "[]" %% TVar "b"))
+    ]
+    where
+    infixr 8 -->
+    t --> u = makeArrow t u
+    infix 1 .::
+    (.::) = (,)
+    infixl 9 %%
+    (%%) = TApp

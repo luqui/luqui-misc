6f020886fa251fad85cb219da87aa29b46b3df15
Luke Palmer <lrpalmer@gmail.com>
2008-03-26 09:10:05 +0000

    Made a cabal package for data-accessor.

--- work/code/haskell/modules/data-accessor/Data/Accessor.hs
+++ work/code/haskell/modules/data-accessor/Data/Accessor.hs
@@ -1,6 +1,12 @@
 {-# OPTIONS_GHC -fglasgow-exts -fth #-}
 
-module Accessor
+-- |This module provides a simple abstract data type for
+-- a "piece" of a data stucture that can be read from and
+-- written to.  It provides an automatic Template Haskell
+-- routine to scour data type definitions and generate
+-- accessor objects for them automatically.
+
+module Data.Accessor
     ( Accessor(..)
     , nameDeriveAccessors, deriveAccessors
     , getA, putA, modA
@@ -14,16 +20,22 @@ import Data.Maybe (catMaybes)
 import Control.Monad (guard)
 import Control.Monad.State
 
+-- |An @Accessor s a@ is an object that encodes how to
+-- get and put a subject of type @a@ out of/into an object
+-- of type @s@.
+-- 
+-- In order for an instance of this data structure @a@ to be
+-- an "Accessor", it must obey the following laws:
+-- 
+-- > getVal a (setVal a x s) = x
+-- > setVal a (getVal a s) s = s
 data Accessor s a
     = Accessor { getVal :: s -> a
                , setVal :: a -> s -> s
                }
 
--- must satisfy the following laws:
--- getVal a (setVal a x s) == x
--- setVal a (getVal a s) s == s
-
 infixl 9 .> 
+-- |Accessor composition.
 (.>) :: Accessor a b -> Accessor b c -> Accessor a c
 f .> g = 
     Accessor { getVal = getVal g . getVal f
@@ -31,22 +43,53 @@ f .> g =
              }
 
 infixr 9 <.
+-- |Accessor composition the other direction. 
+-- 
+-- > (<.) = flip (.>)
 (<.) :: Accessor b c -> Accessor a b -> Accessor a c
 (<.) = flip (.>)
 
 infix 1 =:
+-- |An "assignment operator" for state monads.  
+--
+-- > (=:) = putA
 (=:) :: MonadState s m => Accessor s a -> a -> m ()
 (=:) = putA
 
+-- |A structural dereference function for state monads.
 getA :: MonadState s m => Accessor s a -> m a
 getA a = liftM (getVal a) get
 
+-- |A structural assignment function for state monads.
 putA :: MonadState s m => Accessor s a -> a -> m ()
 putA a x = get >>= put . setVal a x
 
+-- |A structural modification function for state monads.
 modA :: MonadState s m => Accessor s a -> (a -> a) -> m ()
 modA a f = liftM f (getA a) >>= putA a
 
+-- |@deriveAccessors n@ where @n@ is the name of a data type
+-- declared with @data@ looks through all the declared fields
+-- of the data type, and for each field ending in an underscore
+-- generates an accessor of the same name without the underscore.
+--
+-- It is @nameDeriveAccessors n f@ where @f@ satisfies 
+-- > f (s ++ "_") = Just s
+-- > f x          = Nothing   -- otherwise
+--
+-- For example, given the data type:
+--
+-- > data Score = Score { p1Score_ :: Int
+--                      , p2Score_ :: Int
+--                      , rounds   :: Int
+--                      }
+--
+-- @deriveAccessors@ will generate the following objects:
+--
+-- > p1Score :: Accessor Score Int
+-- > p1Score = Accessor p1Score_ (\x s -> s { p1Score_ = x })
+-- > p2Score :: Accessor Score Int
+-- > p2Score = Accessor p2Score_ (\x s -> s { p2Score_ = x })
 deriveAccessors :: Name -> Q [Dec]
 deriveAccessors n = nameDeriveAccessors n transformName
     where
@@ -55,6 +98,11 @@ deriveAccessors n = nameDeriveAccessors n transformName
         guard $ last s == '_'
         return $ init s
 
+-- |@nameDeriveAccessors n f@ where @n@ is the name of a data type
+-- declared with @data@ and @f@ is a function from names of fields
+-- in that data type to the name of the corresponding accessor. If
+-- @f@ returns @Nothing@, then no accessor is generated for that
+-- field.
 nameDeriveAccessors :: Name -> (String -> Maybe String) -> Q [Dec]
 nameDeriveAccessors t namer = do
     TyConI (DataD _ name _ cons _) <- reify t

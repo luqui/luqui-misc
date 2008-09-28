179514ea089320748f96927740ab2d081c9b2a81
Luke Palmer <lrpalmer@gmail.com>
2008-09-28 17:54:10 -0600

    Added IVar, which allows additive blocking on multiple things, like STM, but safe in unsafePerformIO.

---
+++ work/code/haskell/time_marker_frp/Temporal/IVar.hs
@@ -0,0 +1,79 @@
+module Temporal.IVar 
+    ( IVar, new, write
+    , Reader, read, merge
+    , nonblocking, blocking, combination
+    )
+where
+
+import Prelude hiding (read)
+import Control.Concurrent
+import Control.Applicative
+import Control.Monad (ap)
+
+newtype IVar a = IVar (MVar a)
+
+new :: IO (IVar a)
+new = IVar <$> newEmptyMVar
+
+write :: IVar a -> a -> IO ()
+write (IVar var) x = putMVar var x
+
+-- combinationRead returns either Left v, if the value is already available, or
+-- Right v', where v' is a blocking action
+data Reader a = Reader { combination :: IO (Either a (IO a)) }
+
+mapEither :: (a -> c) -> (b -> d) -> Either a b -> Either c d
+mapEither f g = either (Left . f) (Right . g)
+
+instance Functor Reader where
+    fmap f (Reader io) = Reader (fmap (mapEither f (fmap f)) io)
+    
+instance Monad Reader where
+    return = Reader . return . Left
+    m >>= f = Reader $ do
+        choice <- combination m
+        case choice of
+            Left x -> combination (f x)
+            Right io -> return . Right $ do
+                x <- io
+                either return id =<< combination (f x)
+
+instance Applicative Reader where
+    pure = return
+    (<*>) = ap
+
+read :: IVar a -> Reader a
+read (IVar m) = Reader $ do
+    empty <- isEmptyMVar m
+    if empty
+        then return . Right $ readMVar m
+        else Left <$> readMVar m
+
+merge :: Reader a -> Reader a -> Reader a
+merge a b = Reader $ do
+    x <- combination a
+    case x of
+        Left x' -> return $ Left x'
+        Right xio -> do
+            y <- combination b
+            case y of
+                Left y' -> return $ Left y'
+                Right yio -> return . Right $ do
+                    rvar <- newEmptyMVar
+                    xthr <- forkIO $ putMVar rvar =<< xio
+                    ythr <- forkIO $ putMVar rvar =<< yio
+                    result <- readMVar rvar
+                    killThread xthr
+                    killThread ythr
+                    return result
+
+nonblocking :: Reader a -> IO (Maybe a)
+nonblocking r = do
+    c <- combination r
+    case c of
+        Left x -> return $ Just x
+        Right _ -> return Nothing
+
+blocking :: Reader a -> IO a
+blocking r = either return id =<< combination r
+

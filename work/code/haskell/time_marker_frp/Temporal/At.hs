1b2aee546fcc2aae71ee2f80defcec2fc6a74fd3
Luke Palmer <lrpalmer@gmail.com>
2008-10-06 10:55:39 -0600

    Added a few prims.

--- work/code/haskell/time_marker_frp/Temporal/At.hs
+++ work/code/haskell/time_marker_frp/Temporal/At.hs
@@ -5,8 +5,28 @@ import Control.Monad.ST.Lazy
 import Control.Arrow hiding (pure)
 import Temporal.DynAction
 import Control.Monad.Fix
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Temporal.IVar as IVar
 
 type TimeRep = Double
 
-newtype At t a = At (DynAction (ST t) TimeRep a)
+newtype Time t = Time (IVar.Reader TimeRep)
+
+newtype At t a = At { runAt :: DynAction (ST t) TimeRep a }
     deriving (Functor,Applicative,Monad,MonadFix)
+
+data t :< t' = FakeLT
+
+switch :: Time t' -> At t (Either (t :< t') (t' :< t))
+switch (Time v) = At self
+    where
+    self = mkDynAction $ \t -> return $
+        case unsafePerformIO $ IVar.nonblocking v of
+            Nothing             -> (Left FakeLT, self)
+            Just t' | t < t'    -> (Left FakeLT, self)
+                    | otherwise -> (Right FakeLT, return (Right FakeLT))
+
+now :: At t (Time t)
+now = At self
+    where
+    self = mkDynAction $ \t -> return (Time (return t), self)
